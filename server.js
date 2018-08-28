29640e73260dbfb9631d1abfef40e4012ea0f29e
Daniel Frey <dann.frey@me.com>
2018-08-28 12:21:33 -0700

    server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
-app.use(cors());
+// app.use(cors());
 
 app.use( express.static('./public') );
 
