67890aabc05e8de350b071f6448f96bedbfb2d01
Daniel Frey <dann.frey@me.com>
2018-08-29 11:14:28 -0700

    creating css

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-const CONSTRING = process.env.DATABASE_URL || 'postgres://localhost:5432/postgresql-animated-22077'
+const CONSTRING = process.env.DATABASE_URL || 'postgres://localhost:5432/books_app'
 
 const client = new pg.Client(CONSTRING);
 client.connect();
 client.on('error', err => console.error(err));
 
 app.get('/', (request, response) => {
-  let SQL = 'SELECT title, author, image_url FROM books';
+  let SQL = 'SELECT title, author, description, image_url FROM books';
   client.query(SQL)
     .then( data => {
       let bookData = data.rows;
@@ -34,7 +34,7 @@ function throwDatabaseError(response, error) {
 };
 
 //added __dirname (from code review)
-app.use( express.static(__dirname + './public') );
+app.use( express.static(__dirname + '/public') );
 
 //added (from code review) I think this should be a 404. It's the last thing the load will look at & ONLY if needed
 app.use('*', (request, response) => response.render('pages/error') );
